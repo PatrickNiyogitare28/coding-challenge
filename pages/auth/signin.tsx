d945684c656f840d78a70fceea31cf16b2f36aa8
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2021-12-10 12:47:56 +0200

    chore: added auth page login

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { SigninForm, SignupForm } from '@components/forms';
 import styles from './styles.module.scss';
 import { useRouter } from 'next/router';
+import { useAppDispatch } from '@redux/store';
 
 const Signin: React.FC = () => {
     const router: any = useRouter();
+    const dispatch: any = useAppDispatch();
+    const [theme, setTheme] = useState<string>('light')
+    useEffect(() => {
+        const theme: any = (typeof window !== 'undefined') ? localStorage.getItem('theme') : 'light';
+        document.body.dataset.theme = theme
+        setTheme(theme)
+    },[dispatch])
     return (
         <div className={styles.viewContainer}>
             <div className={styles.highrightWrapper}> 
                 <div className={styles.socialMediaAuthWrapper}>
-                <h1>
+                <h1  style={{color: theme === 'dark' ? 'white':''}}>
                     Login with your Account to Continue
                 </h1>
                 <button>
--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import Image from 'next/image';
 import { SignupForm } from '@components/forms';
 import styles from './styles.module.scss';
 import { useRouter } from 'next/router';
+import { useAppDispatch } from '@redux/store';
 
 const Signup: React.FC = () => {
+    const dispatch: any = useAppDispatch();
+    const [theme, setTheme] = useState<string>('light')
+    useEffect(() => {
+        const theme: any = (typeof window !== 'undefined') ? localStorage.getItem('theme') : 'light';
+        document.body.dataset.theme = theme
+        setTheme(theme)
+    },[dispatch])
+
     const router:any = useRouter();
     return (
         <div className={styles.viewContainer}>
             <div className={styles.highrightWrapper}> 
                 <div className={styles.socialMediaAuthWrapper}>
-                <h1>
+                <h1 style={{color: theme === 'dark' ? 'white':''}}>
                     Create Account to Continue
                 </h1>
                 <button>
